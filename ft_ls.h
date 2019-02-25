b100d1b8e00c999a2f6e2605e23866f6f98938cb
MixFon <mixfon@rambler.ru>
2019-02-25 20:08:56 +0300

    ft_ls

---
+++ ft_ls.h
@@ -0,0 +1,6 @@
+#ifndef FT_LS_H
+#define FT_LS_H
+
+#include "libft_last_ver/ft_printf.h"
+
+#endif
--- main.c
+++ main.c
@@ -1,15 +1,35 @@
-#include <stdio.h>
+#include "ft_ls.h"
 #include <sys/types.h>
 #include <dirent.h>
 
-int main (int ac, char **av)
+void	ft_open_dir(char *name_dir)
 {
 	DIR				*dir;
 	struct dirent	*dirent;
 
-	dir = opendir("/");
+	if (!(dir = opendir(name_dir)))
+	{
+		ft_printf("No open %s", name_dir);
+		return ;
+	}
 	while ((dirent = readdir(dir)) != NULL)
-		printf("%s\n", dirent->d_name);
+		ft_printf("%s\n", dirent->d_name);
 	closedir(dir);
+}
+
+
+int main (int ac, char **av)
+{
+	int i;
+
+	i = 1;
+	if (ac == 1)
+		ft_open_dir(".");
+	else 
+		while (i < ac)
+		{
+			ft_open_dir(av[i]);
+			i++;
+		}
 	return (0);
 }
